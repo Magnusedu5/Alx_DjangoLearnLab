360d9572e150f3f32f1d81e4a802ff247916118e
Magnus Edu <magnusedu5@gmail.com>
2025-07-19 00:20:22 +0100

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,3 +6,27 @@ urlpatterns = [
     path('list_books/', views.list_books, name='list_books'),
     path('view_books/<int:pk>/', views.LibraryDetailView.as_view(), name='view_books')
 ]
+
+#user login
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+from .views import RegisterView
+   
+
+urlpatterns = [
+    path('logout/', 
+         LogoutView.as_view(template_name='relationship_app/logout.html'), 
+         name='logout'
+         ),
+
+    path('register/', 
+         RegisterView.as_view(), 
+         name='register'
+         ),
+
+     path('login/', 
+         LoginView.as_view(template_name='relationship_app/login.html'), 
+         name='login'
+         ),
+
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,3 +11,18 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
+
+#user registration
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
+
+
+    def get(self, request, *args, **kwargs):
+        print("GET request received")  # This will show in your terminal
+        return super().get(request, *args, **kwargs)
