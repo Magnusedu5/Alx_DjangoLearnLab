2da3452c92a862f9f00269a43ab13d09d7f1a56c
Magnus Edu <magnusedu5@gmail.com>
2025-08-22 01:22:55 +0100

    Adding Comment Functionality to Blog Posts

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile, Post, Comment
+from taggit.forms import TagWidget
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -31,9 +32,9 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ["title", "content", "tags"]
         widgets = {
-            "tags": forms.TextInput(attrs={"placeholder": "Add tags separated by commas"}),
+            "tags": TagWidget(),
         }
-        
+
     def validate_title(self):
         title = self.cleaned_data.get("title")
         if len(title) < 5:
