ba61781c6f786735590f9736021bceaa8b718259
Magnus Edu <magnusedu5@gmail.com>
2025-09-15 11:31:52 +0100

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,13 +5,13 @@ from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer, UserSerializer
 from django.shortcuts import get_object_or_404
+from models import CustomUser
 
 
-User = get_user_model()
 
 # Register new user
 class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
+    queryset = CustomUser.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
 
@@ -31,11 +31,13 @@ class ProfileView(generics.RetrieveUpdateAPIView):
         return self.request.user  # Always return the logged-in user
 
 
-class FollowUserView(views.APIView):
+class FollowUserView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        target_user = get_object_or_404(User, id=user_id)
+        target_user = get_object_or_404(CustomUser, id=user_id)
         
         if target_user == request.user:
             return Response({"detail": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
@@ -44,11 +46,13 @@ class FollowUserView(views.APIView):
         return Response({"detail": f"You are now following {target_user.username}."}, status=status.HTTP_200_OK)
 
 
-class UnfollowUserView(views.APIView):
+class UnfollowUserView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer    
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        target_user = get_object_or_404(User, id=user_id)
+        target_user = get_object_or_404(CustomUser, id=user_id)
 
         if target_user == request.user:
             return Response({"detail": "You cannot unfollow yourself."}, status=status.HTTP_400_BAD_REQUEST)
